1e778a5570f77f6a0232639515486a3150b35a64
user <>
2018-08-06 10:55:54 +0900

    init

---
+++ pack.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+coin=yangcoin
+version=0.14.2
+platform=`uname -a | awk '{print tolower($1)}'`
+arch=`uname -m`
+
+if test "${platform}" = "linux" ; then
+    if test "${arch}" = "x86_64" ; then
+        tarball_name="${coin}-${version}-linux64.tar.gz"
+    elif test "${arch}" = "x86_32" ; then
+        tarball_name="${coin}-${version}-linux32.tar.gz"
+    fi
+elif test "${platform}" = "darwin" ; then
+    tarball_name="${coin}-${version}-osx64.tar.gz"
+else
+    echo "${coin} binary distribution not available for platform and architecture"
+    exit -1
+fi
+bitcoin_dir=${coin}-${version}
+rm -Rf tmp/*
+rm -Rf ${tarball_name}
+
+mkdir -p tmp/${coin}
+
+cp src/${coin}-cli          tmp/${coin}/${coin}-cli
+cp src/${coin}-tx           tmp/${coin}/${coin}-tx
+cp src/${coin}d             tmp/${coin}/${coin}d
+cp src/test/test_bitcoin    tmp/${coin}/test_${coin}
+cp src/wallet-utility       tmp/${coin}/
+cp src/qt/${coin}-qt        tmp/${coin}/${coin}-qt
+cp ${coin}.conf             tmp/${coin}/${coin}.conf
+cd tmp
+tar -zcvf ../${tarball_name} ./
+cd ../
