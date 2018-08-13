c3a186a516947a84967aa4b3505ba5af125aaf75
user <>
2018-08-13 12:38:30 +0900

    sd

---
+++ maketmp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -Rf ../tmp/*
+mkdir ../tmp/data
+cp -f ./jbcoin.tmp.conf ../tmp/data/jbcoin.conf
+cp src/jbcoind ../tmp/
+cp src/qt/jbcoin-qt ../tmp/
+
--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -119,7 +119,7 @@ public:
         consensus.BIP34Height = 0;
         consensus.BIP65Height = 0; // bab3041e8977e0dc3eeff63fe707b92bde1dd449d8efafb248c27c8264cc311a
         consensus.BIP66Height = 0; // 7aceee012833fa8952f8835d8b1b3ae233cd6ab08fdb27a771d2bd7bdc491894
-        consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); 
+        consensus.powLimit = uint256S("0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); 
         consensus.nPowTargetTimespan = 1 * 1 * 60 * 60; // 1 hour  sec
         consensus.nPowTargetSpacing = 1 * 60;//1min
         consensus.fPowNoRetargeting = false;
